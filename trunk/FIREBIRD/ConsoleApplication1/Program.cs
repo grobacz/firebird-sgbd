57c7e5a2ea09bcb0e44e620a869eb97e114055ee
arrr.cin@gmail.com <arrr.cin@gmail.com@2208fd68-15be-ab1b-a995-1a01182f5eae>
2011-06-26 01:56:24 +0000

    INIT

---
+++ trunk/FIREBIRD/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FirebirdSql.Data.FirebirdClient;
+using System.IO;
+
+namespace TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(File.Exists(Path.Combine("data", "SGBD.FDB")));
+
+            
+
+            FbConnection con = new FbConnection("User ID=sysdba;Password=290389;" + 
+               "Database=\\data\\SGBD.FDB; " + 
+               "DataSource=localhost;Charset=NONE;");
+
+            //con.Open();
+        }
+    }
+}
